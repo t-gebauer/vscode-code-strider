8acfd6b4a6133651cce4a2ee7b344da8e6bf09e8
T.Gebauer <t-gebauer@users.noreply.github.com>
2020-10-25 17:38:10 +0100

    basic navigation

--- src/activation.ts
+++ src/activation.ts
@@ -1,5 +1,5 @@
 import { TextEditor } from "vscode";
-import { parseJS } from "./javascript";
+import { initEditor, parseJS } from "./javascript";
 
 export function handleEditorChange(textEditor: TextEditor | undefined) {
     console.log('Editor changed');
@@ -8,6 +8,7 @@ export function handleEditorChange(textEditor: TextEditor | undefined) {
     if (textEditor?.document.languageId === 'javascript') {
         console.log('its JS!');
 
+        initEditor(textEditor);
         parseJS(textEditor.document.getText());
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { handleEditorChange } from './activation';
+import { nextSibling, previousSibling, gotoParent, gotoFirstChild } from './javascript';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -23,10 +24,30 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('code-strider.firstChild', (editor) => {
+			//vscode.window.showInformationMessage('command: firstChild');
+			gotoFirstChild(editor);
+		}));
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('code-strider.gotoParent', (editor) => {
+			//vscode.window.showInformationMessage('command: gotoParent');
+			gotoParent(editor);
+		}));
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('code-strider.nextSibling', (editor) => {
+			//vscode.window.showInformationMessage('command: nextSibling');
+			nextSibling(editor);
+		}));
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('code-strider.previousSibling', (editor) => {
+			//vscode.window.showInformationMessage('command: previousSibling');
+			previousSibling(editor);
+		}));
 
 	vscode.window.onDidChangeActiveTextEditor(handleEditorChange);
 	handleEditorChange(vscode.window.activeTextEditor);
 }
 
-// this method is called when your extension is deactivated
+// this method is caled when your extension is deactivated
 export function deactivate() { }
