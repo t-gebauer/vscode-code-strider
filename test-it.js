7701304a3ebb9653d30c8c2d93868a72f7d17c73
T.Gebauer <t-gebauer@users.noreply.github.com>
2020-12-08 18:41:31 +0100

    Format all .json and .js files

--- test-it.js
+++ test-it.js
@@ -1,10 +1,10 @@
-const Parser = require('tree-sitter');
-const JavaScript = require('tree-sitter-javascript');
+const Parser = require("tree-sitter")
+const JavaScript = require("tree-sitter-javascript")
 
-const parser = new Parser();
-parser.setLanguage(JavaScript);
+const parser = new Parser()
+parser.setLanguage(JavaScript)
 
-const sourceCode = 'let x = 1; console.log(x);';
-const tree = parser.parse(sourceCode);
+const sourceCode = "let x = 1; console.log(x);"
+const tree = parser.parse(sourceCode)
 
-console.log(tree.rootNode.toString());
+console.log(tree.rootNode.toString())
