48e044b0d1197e8522a335a7ee9e93879dc747cc
T.Gebauer <t-gebauer@users.noreply.github.com>
2021-01-01 13:57:11 +0100

    remove duplication between nodeAbove and nodeBelow
    Also makes the algorithm easier to understand by hiding complexity

--- src/spatial-movement-commands.ts
+++ src/spatial-movement-commands.ts
@@ -1,90 +1,92 @@
 import { TextEditor, TextEditorEdit } from "vscode"
 import { SyntaxNode } from "web-tree-sitter"
 import { EditorState } from "./editor-state"
-import { EditorStateChange, logger } from "./extension"
+import { EditorStateChange } from "./extension"
 
 // --- spatial movement functions (natural, planar)
 
-function findNodeWithParentContainingLine(node: SyntaxNode, line: number): SyntaxNode | false {
+function findNodeWithParentContainingLine(
+    node: SyntaxNode,
+    line: number,
+    forward = true
+): SyntaxNode | false {
     while (true) {
         if (!node.parent) {
             return false
         }
-        if (node.parent.startPosition.row <= line || node.parent.endPosition.row >= line) {
+        if (forward ? node.parent.endPosition.row >= line : node.parent.startPosition.row <= line) {
             return node
         }
         node = node.parent
     }
 }
 
-export function nodeAbove(node: SyntaxNode, force = false): SyntaxNode | undefined {
-    const targetLine = node.startPosition.row - 1
+function nextSibling(node: SyntaxNode, forward = true) {
+    return node[forward ? "nextNamedSibling" : "previousNamedSibling"]
+}
+
+function nextChild(node: SyntaxNode, forward = true) {
+    return node[forward ? "firstNamedChild" : "lastNamedChild"]
+}
+
+function findSiblingContainingLine(
+    node: SyntaxNode,
+    line: number,
+    forward = true
+): SyntaxNode | false {
+    while (forward ? node.endPosition.row < line : node.startPosition.row > line) {
+        const sibling = nextSibling(node, forward)
+        if (!sibling) {
+            return false
+        }
+        node = sibling
+    }
+    return node
+}
+
+function findNextSiblingOfParent(node: SyntaxNode, forward = true): SyntaxNode {
+    while (node.parent && !nextSibling(node, forward)) {
+        node = node.parent
+    }
+    return nextSibling(node, forward) || node
+}
+
+// Find the next node which starts on the line below|above this one
+// Will step outside of the bounds of this nodes parent if `force` is set.
+function nextNode(node: SyntaxNode, forward = true, force = false): SyntaxNode | undefined {
+    const targetLine = forward ? node.endPosition.row + 1 : node.startPosition.row - 1
     // go up, until a parent contains the searched line
-    const parent = findNodeWithParentContainingLine(node, targetLine)
+    const parent = findNodeWithParentContainingLine(node, targetLine, forward)
     if (!parent) {
         return node
     }
     node = parent
     // search in siblings and their children
     while (true) {
-        // traverse the siblings until we find the one, which contains the searched line
-        while (node.startPosition.row > targetLine) {
-            if (!node.previousNamedSibling) {
-                // no more siblings left
-                if (!force) return node
-                while (node.parent && !node.previousNamedSibling) {
-                    node = node.parent
-                }
-                return node.previousNamedSibling || node
-            }
-            node = node.previousNamedSibling
+        const sibling = findSiblingContainingLine(node, targetLine, forward)
+        if (!sibling) {
+            return force ? findNextSiblingOfParent(node, forward) : node
         }
-        // we found a sibling containing our target line
-        if (node.endPosition.row <= targetLine) {
+        node = sibling
+        // We found a sibling containing our target line
+        if (forward ? node.startPosition.row >= targetLine : node.endPosition.row <= targetLine) {
             return node
         }
-        // we found a sibling containing our target line, but it starts too early
-        if (!node.lastNamedChild) {
+        // we found a sibling containing our target line, but it (starts too early|ends too late)
+        const child = nextChild(node, forward)
+        if (!child) {
             return node
         }
-        node = node.lastNamedChild
+        node = child
     }
 }
 
-// find the next node which starts on the line below this one
-// Will step outside of the bounds of this nodes parent if `force` is set.
+export function nodeAbove(node: SyntaxNode, force = false): SyntaxNode | undefined {
+    return nextNode(node, false, force)
+}
+
 export function nodeBelow(node: SyntaxNode, force = false): SyntaxNode | undefined {
-    const targetLine = node.endPosition.row + 1
-    // go up, until a parent contains the searched line
-    const parent = findNodeWithParentContainingLine(node, targetLine)
-    if (!parent) {
-        return node
-    }
-    node = parent
-    // search in siblings and their children
-    while (true) {
-        // traverse the siblings until we find the one, which contains the searched line
-        while (node.endPosition.row < targetLine) {
-            if (!node.nextNamedSibling) {
-                // no more siblings left
-                if (!force) return node
-                while (node.parent && !node.nextNamedSibling) {
-                    node = node.parent
-                }
-                return node.nextNamedSibling || node
-            }
-            node = node.nextNamedSibling
-        }
-        // we found a sibling containing our target line
-        if (node.startPosition.row >= targetLine) {
-            return node
-        }
-        // we found a sibling containing our target line, but it starts too early
-        if (!node.firstNamedChild) {
-            return node
-        }
-        node = node.firstNamedChild
-    }
+    return nextNode(node, true, force)
 }
 
 function nodeLeftOf(node: SyntaxNode): SyntaxNode | undefined {
